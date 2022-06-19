1b04f739eb6e7fb925e5ebaee11c2a9732393df0
PressTpro <95140308+PressTpro@users.noreply.github.com>
2022-06-19 17:32:48 -0500

    Added Play Solo Feature

--- GoodLauncher.bat
+++ GoodLauncher.bat
@@ -9,11 +9,13 @@ echo 1) Join a Server
 echo 2) Host a Server
 echo 3) Avatar Editor
 echo 4) Help
+echo 5) Play Solo
 set /p selection="Select an Option:"
 if %selection% == 1 start join.bat
 if %selection% == 2 start host.bat
 if %selection% == 3 start avatar_editor.bat
 if %selection% == 4 goto help
+if %selection% == 5 goto playsolo
 :help
 cls
 title GoodLauncher Help Center
@@ -22,3 +24,13 @@ echo info@goodrooms.cf
 echo Type 1 to return to Homepage
 set /p return="Return to Homepage:"
 if %return% == 1 goto menu
+:playsolo
+cls
+title GoodLauncher Play Solo
+echo you can play Solo by Running this piece of script
+echo First you need to put the place you will run in Play Solo Mode
+set /p place="Locate a Place from your Computer:"
+echo Setting up...
+echo local player = game.Players:CreateLocalPlayer(0) >>PlaySolo.lua
+echo player:LoadCharacter() >>PlaySolo.lua
+start GoodRoomsApp.exe %script% /content/PlaySolo.lua
